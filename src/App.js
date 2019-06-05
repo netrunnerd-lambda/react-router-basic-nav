e2c0a2994685106f6c8ca646a7e3797cd577256f
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-06-05 16:35:45 -0400

    finished the basic nav (links and routes)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-  <div>
+  <Fragment>
     <Navigation />
-  </div>
+    <Route exact component={Home} path="/" />
+    <Route component={About} path="/about" />
+    <Route component={Contact} path="/contact" />
+  </Fragment>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = _ => (
+  <Router>
+    <App />
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
